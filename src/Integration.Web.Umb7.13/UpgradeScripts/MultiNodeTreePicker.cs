37753618005903fa667e5572c5b1ffde63f9d4a6
Peter Edney <peter@programystic.com>
2019-03-01 19:29:13 +0000

    v7.13 integration test website

---
+++ src/Integration.Web.Umb7.13/UpgradeScripts/MultiNodeTreePicker.cs
@@ -0,0 +1,31 @@
+﻿using DeployCmsData.Core.Attributes;
+using DeployCmsData.Core.Interfaces;
+using DeployCmsData.UmbracoCms.Builders;
+using DeployCmsData.UmbracoCms.Services;
+using System;
+
+namespace Integration.Web.Umb7._13.UpgradeScripts
+{
+    [RunScriptEveryTime]
+    public class MultiNodeTreePicker : UmbracoUpgradeScript
+    {
+        public override bool RunScript(IUpgradeLogRepository upgradeLog)
+        {
+            var id = Guid.Parse("{60FFCA99-3B98-49EA-9F64-E4C69BB00285}");
+
+            Library.DeleteDataTypeById(id);
+            var builder = new MultiNodeTreePickerBuilder(id);
+
+            builder
+                .Name("Another Multi Node Tree Picker")
+                .AllowItemsOfType("type1", "type2")
+                .MinimumNumberOfItems(1)
+                .MaximumNumberOfItems(5)
+                .ShowOpenButton()
+                //.StartNodeContent(1234)
+                .Build();
+
+            return true;
+        }
+    }
+}
