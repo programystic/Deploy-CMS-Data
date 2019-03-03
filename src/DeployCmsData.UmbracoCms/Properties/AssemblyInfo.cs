88de073e033d74db99bc6d8f5450f9ed4ca77139
Peter Edney <peter@programystic.com>
2019-03-03 16:26:21 +0000

    Upfate version number v7.4.0 (not beta)

--- src/DeployCmsData.UmbracoCms/Properties/AssemblyInfo.cs
+++ src/DeployCmsData.UmbracoCms/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Programystic")]
 [assembly: AssemblyProduct("DeployCmsData.UmbracoCms")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("7.4.0.0")]
-[assembly: AssemblyInformationalVersion("7.4.0.0-beta")]
+//[assembly: AssemblyInformationalVersion("7.4.0.0-beta")]
