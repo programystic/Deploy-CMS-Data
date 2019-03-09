b8866b0ad59ab85290150cb60d3c11e85b9f7b82
Peter Edney <peter@programystic.com>
2019-03-09 08:36:55 +0000

    Add upgradescripts v8.0 project

---
+++ src/DeployCmsData.UpgradeScripts.8.0/ApiController/IntegrationTestBaseController.cs
@@ -0,0 +1,34 @@
+﻿using DeployCmsData.Core.Data;
+using DeployCmsData.Core.Interfaces;
+using DeployCmsData.Core.Services;
+using DeployCmsData.UmbracoCms.Data;
+using System.Reflection;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace DeployCmsData.UpgradeScripts_8.ApiController
+{
+    // TODO : secure this controller - UmbracoAuthorizedApiController
+    public class IntegrationTestBaseController : UmbracoApiController
+    {
+        private readonly UpgradeLogRepository _logRepository;
+        private readonly UpgradeScriptRepository _scriptRepository;
+        private UpgradeScriptManager _upgradeScriptManager;
+
+        public IntegrationTestBaseController()
+        {
+            _logRepository = new UpgradeLogRepository();
+            _scriptRepository = new UpgradeScriptRepository();
+            _upgradeScriptManager = new UpgradeScriptManager(_logRepository, _scriptRepository);
+        }
+
+        [HttpGet]
+        public bool RunScript(string scriptName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var upgradeScript = (IUpgradeScript)assembly.CreateInstance($"DeployCmsData.UpgradeScripts_8.UpgradeScripts.{scriptName}");
+            var log = _upgradeScriptManager.RunScript(upgradeScript);
+            return log.Success;
+        }
+    }
+}
---
+++ src/DeployCmsData.UpgradeScripts.8.0/UpgradeScripts/ClearTheDecks.cs
@@ -0,0 +1,22 @@
+﻿using DeployCmsData.Core.Attributes;
+using DeployCmsData.Core.Interfaces;
+using DeployCmsData.UmbracoCms.Services;
+
+namespace DeployCmsData.UpgradeScripts_8.UpgradeScripts
+{
+    [DoNotAutoRun]
+    public class ClearTheDecks : IUpgradeScript
+    {
+        public bool RunScript()
+        {
+            var library = new UmbracoLibrary();
+
+            library.DeleteAllContent();
+            library.DeleteAllDocumentTypes();
+            library.DeleteAllDocumentTypeFolders();
+            //Library.DeleteAllTemplates();
+
+            return true;
+        }
+    }
+}
