a170ad43b88d9f957810571ba1cba0d8b5f460d4
Peter Edney <peter@programystic.com>
2019-03-27 20:13:20 +0000

    Grid row configuration and document type composition inheritance

---
+++ src/DeployCmsData.UpgradeScripts.7.0/UpgradeScripts/DocTypeInheritance.cs
@@ -0,0 +1,37 @@
+﻿using DeployCmsData.Core.Attributes;
+using DeployCmsData.Core.Interfaces;
+using DeployCmsData.UmbracoCms.Builders;
+using System.Linq;
+using Validation;
+
+namespace DeployCmsData.UpgradeScripts_7.UpgradeScripts
+{
+    [DoNotAutoRun]
+    public class DocTypeInheritance : IUpgradeScript
+    {
+        public bool RunScript()
+        {
+            var builder1 = new DocumentTypeBuilder("websiteRoot");
+            builder1.AddField("Title")
+                .Tab("Website Root");
+            builder1.Update();
+
+            var builder2 = new DocumentTypeBuilder("website2");
+            builder2.AddField("Title 2")
+                .Tab("Web Site 2");
+            builder2.BuildWithParent("websiteRoot");
+
+            var builder3 = new DocumentTypeBuilder("website3");
+            builder3.AddField("Title 3")
+                .Tab("Web Site 3");
+            builder3.BuildWithParent("website2");
+
+            var docType = new DocumentTypeBuilder("inheritedDocTypeTest").
+                BuildWithParent("website3");
+
+            Verify.Operation(docType.CompositionAliases().Count() == 3, "We should have 3 composition aliases");
+
+            return true;
+        }
+    }
+}
