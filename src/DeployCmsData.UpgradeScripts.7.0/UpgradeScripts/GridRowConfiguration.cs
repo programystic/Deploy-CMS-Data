17fd648ad71402e6e4bfef38f68a0859c216da84
Peter Edney <peter@programystic.com>
2019-03-27 21:21:22 +0000

    Integration tests updates - grid row configuration

--- src/DeployCmsData.UpgradeScripts.7.0/UpgradeScripts/GridRowConfiguration.cs
+++ src/DeployCmsData.UpgradeScripts.7.0/UpgradeScripts/GridRowConfiguration.cs
@@ -11,23 +11,27 @@ namespace DeployCmsData.UpgradeScripts_7.UpgradeScripts
         public bool RunScript()
         {
             new GridDataTypeBuilder(new Guid("{B0F71D78-745E-4BE5-925E-FA087D44C919}"))
+                .Name("grid 1")
                 .AddAreaToRow("row 1", 2)
                 .AddAreaToRow("row 1", 4)
                 .AddAreaToRow("row 1", 6)
                 .Build();
 
             new GridDataTypeBuilder(new Guid("{9447F9CF-2112-45CD-933B-6F8F6411EF65}"))
+                .Name("grid 2")
                 .AddRow("row 1", 2, 4)
                 .AddAreaToRow("row 1", 6)
                 .Build();
 
             new GridDataTypeBuilder(new Guid("{D35416BC-58FC-4DB3-B67E-B7111BDA8CAA}"))
+                .Name("grid 3")
                 .AddAreaToRow("row 1", 2, 10)
                 .AddAreaToRow("row 1", 4, 20)
                 .AddAreaToRow("row 1", 6, 30)
                 .Build();
 
             new GridDataTypeBuilder(new Guid("{4E3F4D54-3900-43A7-B37F-ABDA1802631E}"))
+                .Name("grid 4")
                 .AddAreaToRow("row 1", 2, 10, "rte", "headline", "image")
                 .AddAreaToRow("row 1", 4, 20, "image")
                 .AddAreaToRow("row 1", 6, 30, "headline", "rte")
